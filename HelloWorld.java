2589d5de819fb7d4f93a2613424f28952d745f2e
Iaskey <31746283+Iaskey@users.noreply.github.com>
2017-09-07 19:22:20 -0500

    Name
    Names

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author IsaacAskey
+ * @date 09/06/17
+ * @course CSCE155H
+ * 
+ * @description This program is a simple println example.
+ */
+
+
+public class HelloWorld {
+
+	public static void main( String[] args ){
+	
+		System.out.println("Isaac Askey");
+	
+	}
+}
